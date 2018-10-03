287bf7b148e149ce5395e850e327a136fac2b5d9
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2018-10-04 00:29:04 +0200

    Finished add clients feature

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 class AddClient extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    balance: ""
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newClient = this.state;
+
+    const { firestore, history } = this.props;
+
+    // If no balance, make 0
+    if (newClient.balance === "") {
+      newClient.balance = 0;
+    }
+
+    firestore
+      .add({ collection: "clients" }, newClient)
+      .then(() => history.push("/"));
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     return (
       <div>
@@ -16,8 +47,8 @@ class AddClient extends Component {
         <div className="card">
           <div className="card-header">Add Client</div>
           <div className="card-body">
-            <form>
-              <div className="card-header">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
                 <label htmlFor="firstName">First Name</label>
                 <input
                   className="form-control"
@@ -25,8 +56,64 @@ class AddClient extends Component {
                   name="firstName"
                   minLength="2"
                   required
+                  onChange={this.onChange}
+                  value={this.state.firstName}
                 />
               </div>
+
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  name="lastName"
+                  minLength="2"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.lastName}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  className="form-control"
+                  type="email"
+                  name="email"
+                  onChange={this.onChange}
+                  value={this.state.email}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  name="phone"
+                  minLength="10"
+                  required
+                  onChange={this.onChange}
+                  value={this.state.phone}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="balance">Balance</label>
+                <input
+                  className="form-control"
+                  type="number"
+                  name="balance"
+                  onChange={this.onChange}
+                  value={this.state.balance}
+                />
+              </div>
+
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-primary btn-block"
+              />
             </form>
           </div>
         </div>
@@ -35,4 +122,8 @@ class AddClient extends Component {
   }
 }
 
-export default AddClient;
+AddClient.PropTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
--- src/store.js
+++ src/store.js
@@ -25,7 +25,9 @@ const rrfConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Init firestore
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+const settings = { timestampsSnapshots: true };
+firestore.settings(settings);
 
 // Add reactReduxFirebase enhancer when making store creator
 const createStoreWithFirebase = compose(
