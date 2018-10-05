0e40135abbc695c09ad0dff26ca98d21de19897a
Nikola Trajkovic <nikolatrajkovic.contact@gmail.com>
2018-10-05 22:11:34 +0200

    First version of Client Details page

---
+++ src/components/clients/ClientDetails.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { Sipnner } from "../layout/Spinner";
+
+class ClientDetails extends Component {
+  render() {
+    const { client } = this.props;
+
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-alt-circle-left" /> Back To Dashboard
+              </Link>
+            </div>
+            <div className="col-md-6">
+              <div className="btn-group float-right">
+                <Link to={`client/edit${client.id}`} className="btn btn-dark">
+                  Edit
+                </Link>
+                <button className="btn btn-danger">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Sipnner />;
+    }
+  }
+}
+
+export default compose(
+  firestoreConnect([
+    props => [
+      { collection: "clients", storeAs: "client", doc: props.match.params.id }
+    ]
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
